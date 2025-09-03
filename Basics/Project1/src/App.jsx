f136d23675ed4caaa8c99159a25933c165d75b48
Ayush Mishra <ayushbm84@gmail.com>
2025-09-03 10:53:31 +0530

    practicing minor porjects

--- Basics/Project1/src/App.jsx
+++ Basics/Project1/src/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import Products from './Products.jsx';
 import State from './State.jsx';
+import Hero from './components/blocks/hero';
+
 
 export default function App() {
-  return (
-    <div className='w-full h-screen bg-zinc-900'>
-        <div className='flex items-center justify-center h-full bg-gradient-to-r from-purple-500 to-pink-500'>
-            <h1 className="text-3xl font-bold text-white">Hello World!</h1>
+    return (
+        <div>
+
+            <Hero />
         </div>
-        <Products />
-        <State />
-    </div>
-  )
+    )
 }
+
+
+// <div className='w-full h-screen bg-zinc-900'>
+//     <div className='flex items-center justify-center h-full bg-gradient-to-r from-purple-500 to-pink-500'>
+//         <h1 className="text-3xl font-bold text-white">Hello World!</h1>
+//     </div>
+//     <Products />
+//     <State />
+// </div>
